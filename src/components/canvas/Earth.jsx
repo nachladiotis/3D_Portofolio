878752adab186568d20a5ef634fec58bb453f627
Nikos Achladiotis <nikosachlad@gmail.com>
2025-12-16 19:01:31 +0200

    Create Contact Feedback works earth starts

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  const earth = useGLTF("./planet/scene.gltf");
 
-export default Earth
+  return (
+    <primitive
+      object={earth.scene}
+      scale={2.1}
+      position={[0, 0, 0]}
+      rotation-y={0}
+    />
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadowsframeloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          autoRotateSpeed={0.8}
+          enableZoom={false}
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          target={[0, 0, 0]} // πολύ σημαντικό
+        />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
