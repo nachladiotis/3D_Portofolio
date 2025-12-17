79f16bd06ad9044c82fc03372f9f04778ea44209
Nikos Achladiotis <nikosachlad@gmail.com>
2025-12-17 15:17:03 +0200

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/3D_Portofolio/",
   plugins: [react(), tailwindcss()],
-})
+});
